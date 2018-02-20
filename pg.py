45bcd94342edc194f2c6132c696271042b6f8c18
Xinyuan Huang <hxydiana@gmail.com>
2018-02-20 10:05:56 -0800

    remove squeeze

--- pg.py
+++ pg.py
@@ -184,16 +184,16 @@ class PG(object):
     else:
       # TODO: output_activation = self.config.activation
       action_means = build_mlp(self.observation_placeholder, self.action_dim, scope=scope, output_activation=tf.nn.relu)
-      action_means = tf.Print(action_means, [action_means, tf.shape(action_means)], message="action_means")
+      #action_means = tf.Print(action_means, [action_means, tf.shape(action_means)], message="action_means")
       with tf.variable_scope(scope):
         log_std = tf.get_variable("log_std", shape=[self.action_dim], dtype=tf.float32)
-        log_std = tf.Print(log_std, [log_std, tf.shape(log_std)], message="log_std")
+        #log_std = tf.Print(log_std, [log_std, tf.shape(log_std)], message="log_std")
         dist = tf.contrib.distributions.MultivariateNormalDiag(action_means, tf.exp(log_std))
         sample = dist.sample()
-        sample = tf.Print(sample, [sample, tf.shape(sample)], message="sample")
-        self.sampled_action = tf.squeeze(sample, axis=-1)
+        #sample = tf.Print(sample, [sample, tf.shape(sample)], message="sample")
+        self.sampled_action = sample
         self.logprob = dist.log_prob(self.action_placeholder)
-        self.logprob = tf.Print(self.logprob, [self.logprob], message="logprob_continuous")
+        #self.logprob = tf.Print(self.logprob, [self.logprob], message="logprob_continuous")
     #######################################################
     #########          END YOUR CODE.          ############
             
