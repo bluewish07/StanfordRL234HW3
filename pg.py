b7f2ec8fc1a6c1cbe7b003d088bacf7045eafedd
Xinyuan Huang <hxydiana@gmail.com>
2018-02-20 09:53:39 -0800

    debugging

--- pg.py
+++ pg.py
@@ -187,7 +187,7 @@ class PG(object):
         log_std = tf.get_variable("log_std", shape=[self.action_dim], dtype=tf.float32)
         dist = tf.contrib.distributions.MultivariateNormalDiag(action_means, tf.exp(log_std))
         sample = dist.sample()
-        sample = tf.Print(sample, [sample, sample.shape], message="sample")
+        sample = tf.Print(sample, [sample, tf.shape(sample)], message="sample")
         self.sampled_action = tf.squeeze(sample, axis=-1)
         self.logprob = dist.log_prob(self.action_placeholder)
         self.logprob = tf.Print(self.logprob, [self.logprob], message="logprob_continuous")
