8f4272a9ec5722d2d9126f8f1e27e5e99ec8e6aa
Xinyuan Huang <hxydiana@gmail.com>
2018-02-20 22:50:42 -0800

    halfcheetah-0.9-batch_size1000

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class config():
 
     # Change env_name for the different experiments
     #env_name="CartPole-v0"
-    env_name="InvertedPendulum-v1"
-    #env_name="HalfCheetah-v1"
+    #env_name="InvertedPendulum-v1"
+    env_name="HalfCheetah-v1"
 
 
     record           = False
@@ -24,9 +24,11 @@ class config():
     # model and training config
     num_batches = 200 # number of batches trained on
     batch_size = 1000 # number of steps used to compute each policy update
+    #batch_size = 50000
     max_ep_len = 1000 # maximum episode length
     learning_rate = 3e-2
-    gamma              = 1.0 # the discount factor
+    #gamma              = 1.0 # the discount factor
+    gamma = .9
     use_baseline = True
     normalize_advantage=True 
     # parameters for the policy and baseline models
